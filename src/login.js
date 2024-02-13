0537f252304b6d8164c6c1e9caec05f4f05a3f3d
Vinay7995596 <124029698+Vinay7995596@users.noreply.github.com>
2024-02-13 06:40:15 +0530

    Create login.js

---
+++ src/login.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './login.css'
+
+function LoginForm({ onLogin }) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        onLogin(username, password);
+    };
+
+    return (
+        <div className="login">
+            <h4>Login</h4>
+            <div className='form'>
+                <div className='text_area'>
+                <input
+                    className="text_input"
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                </div>
+                <div className='text_area'>
+                <input
+                    className="text_input"
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                </div>
+                <button className="btn" onClick={handleLogin}>Login</button>
+            </div>
+        </div>
+    );
+}
+
+export default LoginForm;
